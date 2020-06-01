13c5022bbd2832476bf4b6bfa56d35e7707d7427
MarcoGuzzetti88 <61061890+MarcoGuzzetti88@users.noreply.github.com>
2020-06-01 16:15:45 +0200

    first

---
+++ precache-manifest.b984f6cb2e6b71b2d1742defea1f988d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f35b10a8426ac460ce5a1e46c4c1d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affc6549767dae4c932a",
+    "url": "/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "affc6549767dae4c932a",
+    "url": "/static/js/2.d18fc162.chunk.js"
+  },
+  {
+    "revision": "9e184fc01facb8510e39",
+    "url": "/static/js/main.23ea6000.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
